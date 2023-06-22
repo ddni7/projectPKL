0def2e68f9f33e30638323a8e7c8ae7fae9d3bf5
ddni7hub <ddni7@users.noreply.github.com>
2023-06-22 23:30:14 +0700

    add new templating login&register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,48 +6,153 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- template css --}}
+    <style>
+        * {
+            font-family: 'Poppins', sans-serif !important;
+        }
+
+        :root {
+            --btn-size: .8rem;
+            --small-size: .75rem;
+        }
+
+        body {
+            background-color: #e4eaeb;
+        }
+
+        .bg {
+            background-color: #E4E8EB;
+        }
+
+        .login-card {
+            width: calc(100% - 13rem);
+        }
+
+        .login-card h1 {
+            color: #111727;
+        }
+
+        .login-card p {
+            /* color: #b0b4b6; */
+            color: #313131;
+            font-size: var(--small-size);
+        }
+
+        .form-control:focus {
+            outline: none;
+            box-shadow: none;
+            border: var(--bs-border-width) solid var(--bs-border-color);
+        }
+
+        .login-card label {
+            font-size: var(--small-size);
+        }
+
+        form.form input {
+            font-size: var(--small-size);
+        }
+
+        form.form input::placeholder {
+            font-size: var(--small-size);
+        }
+
+        form.form button:nth-of-type(1) {
+            color: #ffffff;
+            background-color: #111727;
+            font-size: var(--btn-size);
+        }
+
+        form.form button:nth-last-child(1) {
+            color: #111727;
+            border: 1px solid #111727;
+            font-size: var(--btn-size);
+        }
+
+        form.form button:nth-last-child(2) {
+            color: #111727;
+            border: 1px solid #111727;
+            font-size: var(--btn-size);
+        }
+
+        .checkbox label {
+            font-size: var(--small-size);
+        }
+
+        .checkbox a {
+            font-size: var(--small-size);
+            color: #111727;
+        }
+
+        .signup {
+            font-size: var(--small-size);
+        }
+
+        .signup a {
+            color: #111727;
+        }
+
+        @media (max-width: 480px) {
+            .login-card {
+                width: calc(100% - 2rem);
+            }
+        }
+
+    </style>
+
 </head>
 
 <body>
-    <div class="row justify-content-center mt-2">
-        <div class="col-lg-4">
-            <div class="div text-center">
-                <img src="{{ asset('abc') }}/assets/logo-log.png" alt="">
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="card-title text-center">Login</h1>
-                </div>
-                <div class="card-body">
+    <div class="container py-5 px-5 bg">
+        <div class="row justify-content-between bg-white rounded-start-4">
+            <div class="col-lg-7 d-flex justify-content-center align-items-center">
+                <div class="login-card py-lg-0 py-5">
+                    <h2 class="fw-semibold">Welcome back</h2>
+                    <p class="fw-light">Welcome back ! Please login your Account</p>
+                    {{-- memunculkan pesan error --}}
                     @if(Session::has('error'))
-                    <div class="alert alert-danger" role="alert">
-                        {{ Session::get('error') }}
-                    </div>
+                        <div class="alert alert-danger" role="alert">
+                            {{ Session::get('error') }}
+                        </div>
                     @endif
-                    <form action="{{ route('login') }}" method="POST">
+                    <form action="{{ route('login') }}" method="post" class="form">
                         @csrf
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password" required>
-                        </div>
-                        <div class="div mb-3">
-                            <p>Don't have account? <a href="{{ route('register') }}">Register Account</a></p>
-                        </div>
-                        <div class="mb-3">
-                            <div class="d-grid">
-                                <button class="btn btn-info">Login</button>
+                        <div class="row">
+                            <div class="mb-3 col-12">
+                                <label for="email" class="mb-1 fw-medium">Email</label>
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email" required>
+                            </div>
+                            <div class="col-12">
+                                <label for="password" class="mb-1 fw-medium">Password</label>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password" required>
                             </div>
                         </div>
+                        <button type="submit" class="btn mt-4 w-100">Sign in</button>
+                        <button type="submit" class="btn mt-4 w-100" hidden>Sign in</button>
+                        <p class="text-center signup mt-4">Don't have account? <a href="{{ route('register') }}" class="text-decoration-none fw-semibold">Sign up now</a></p>
                     </form>
                 </div>
+            </div>
+            <div class="col-lg-5 col-12 p-0">
+                <img src="{{ asset('abc') }}/assets/logo-log.png" alt="" class="img-fluid w-100 h-100">
             </div>
         </div>
     </div>
 </body>
+
+{{-- import font --}}
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap');
+
+</style>
+
+{{-- teuing poho deui --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+{{-- cdn js bootsrapp --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
 
 </html>
